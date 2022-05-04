08c1dda486e1c460b7cc8fefb5c43ead979241f1
arsen19899 <83328564+arsen19899@users.noreply.github.com>
2022-05-05 00:09:46 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -25,7 +25,9 @@ const Header = () => {
           {categories.map((category, index) => (
             <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
           ))}
-          <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+
+        </div>
+        <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
             {user && (
                 <div className="flex h-16 flex-row gap-x-2 ">
                   <div className="mt-2">
@@ -41,13 +43,12 @@ const Header = () => {
                   </div>
                 </div>
             )}
-            {!user && (
-                <div className="mt-2">
-                  <Link href={`/api/auth/login`}>Login</Link>
-                </div>
-            )}
+          {!user && (
+              <div className="mt-2">
+                <Link href={`/api/auth/login`}>Login</Link>
+              </div>
+          )}
           </span>
-        </div>
       </div>
     </div>
   );
